d63c1cd78eec98666e83dc4ac33f44ba480c052f
Aman <thakur.aman77@yahoo.com>
2022-07-08 19:06:02 -0400

    Extra - Created a Intro.cshtml.cs file to calculate input numbers and show sum of them to the output - Updated Intro.cshtml to accept IntroModel and update data both inputs and output

--- Assignment-1/Pages/Intro.cshtml.cs
+++ Assignment-1/Pages/Intro.cshtml.cs
@@ -1,9 +1,19 @@
 ﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 namespace Assignment_1.Pages
 {
-	public class Intro
+	public class IntroModel: PageModel
 	{
-		
+        [FromQuery]
+		public double leftNumber { get; set; }
+		[FromQuery]
+		public double rightNumber { get; set; }
+
+		public void OnGet()
+        {
+			ViewData["Result"] = leftNumber + rightNumber;
+        }
 	}
 }
 
